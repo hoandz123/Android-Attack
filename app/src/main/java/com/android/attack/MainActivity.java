9850151e32a42f67e028425e33894cb6fd48b436
Hoan <thanhhoan14082004@gmail.com>
2026-06-01 12:38:31 +0700

    Loader native-only, MainActivity loadLibrary trực tiếp
    - Xóa NativeLoader.java khỏi module loader
    
    - bootstrap JNI export từ libloader.so

--- app/src/main/java/com/android/attack/MainActivity.java
+++ app/src/main/java/com/android/attack/MainActivity.java
@@ -4,21 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.android.attack.loader.NativeLoader;
-
 public class MainActivity extends Activity {
     static {
-        NativeLoader.class.getName();
+        System.loadLibrary("loader");
     }
+
+    private static native void bootstrap(String nativeLibraryDir);
 
     private static native String stringFromNative();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!NativeLoader.loadFromDir(getApplicationInfo().nativeLibraryDir, "attack")) {
-            throw new UnsatisfiedLinkError("loader failed to load libattack.so");
-        }
+        bootstrap(getApplicationInfo().nativeLibraryDir);
         setContentView(R.layout.activity_main);
         ((TextView) findViewById(R.id.sample_text)).setText(stringFromNative());
     }
--- loader/src/main/cpp/loader_entry.cpp
+++ loader/src/main/cpp/loader_entry.cpp
@@ -6,79 +6,28 @@
 #define LOG_TAG "AttackLoader"
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-namespace {
-
-jboolean nativeLoadFromDir(JNIEnv *env, jclass /*clazz*/, jstring nativeLibraryDir, jstring libName) {
-    if (nativeLibraryDir == nullptr || libName == nullptr) {
-        LOGE("nativeLoadFromDir: null argument");
-        return JNI_FALSE;
-    }
-
-    const char *dir = env->GetStringUTFChars(nativeLibraryDir, nullptr);
-    const char *name = env->GetStringUTFChars(libName, nullptr);
-    const bool ok = attack::loader::loadFromDir(env, dir, name);
-
-    if (dir != nullptr) {
-        env->ReleaseStringUTFChars(nativeLibraryDir, dir);
-    }
-    if (name != nullptr) {
-        env->ReleaseStringUTFChars(libName, name);
-    }
-
-    return ok ? JNI_TRUE : JNI_FALSE;
-}
-
-jboolean nativeLoadDownloaded(JNIEnv *env, jclass /*clazz*/, jstring absolutePath) {
-    if (absolutePath == nullptr) {
-        LOGE("nativeLoadDownloaded: null path");
-        return JNI_FALSE;
-    }
-
-    const char *path = env->GetStringUTFChars(absolutePath, nullptr);
-    const bool ok = attack::loader::loadDownloaded(env, path);
-
-    if (path != nullptr) {
-        env->ReleaseStringUTFChars(absolutePath, path);
-    }
-
-    return ok ? JNI_TRUE : JNI_FALSE;
-}
-
-const JNINativeMethod kNativeMethods[] = {
-    {"nativeLoadFromDir", "(Ljava/lang/String;Ljava/lang/String;)Z", reinterpret_cast<void *>(nativeLoadFromDir)},
-    {"nativeLoadDownloaded", "(Ljava/lang/String;)Z", reinterpret_cast<void *>(nativeLoadDownloaded)},
-};
-
-bool registerNatives(JNIEnv *env) {
-    jclass clazz = env->FindClass("com/android/attack/loader/NativeLoader");
-    if (clazz == nullptr) {
-        LOGE("FindClass NativeLoader failed");
-        return false;
-    }
-
-    if (env->RegisterNatives(clazz, kNativeMethods, sizeof(kNativeMethods) / sizeof(kNativeMethods[0])) != JNI_OK) {
-        LOGE("RegisterNatives failed");
-        return false;
-    }
-
-    return true;
-}
-
-} // namespace
-
 extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     if (!attack::loader::bootstrap(vm, reserved)) {
         return JNI_ERR;
     }
-
-    JNIEnv *env = nullptr;
-    if (vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK) {
-        return JNI_ERR;
-    }
-
-    if (!registerNatives(env)) {
-        return JNI_ERR;
-    }
-
     return JNI_VERSION_1_6;
+}
+
+extern "C" JNIEXPORT void JNICALL Java_com_android_attack_MainActivity_bootstrap(JNIEnv *env, jclass /*clazz*/, jstring nativeLibraryDir) {
+    if (nativeLibraryDir == nullptr) {
+        LOGE("bootstrap: null nativeLibraryDir");
+        return;
+    }
+
+    const char *dir = env->GetStringUTFChars(nativeLibraryDir, nullptr);
+    if (dir == nullptr) {
+        LOGE("bootstrap: GetStringUTFChars failed");
+        return;
+    }
+
+    if (!attack::loader::loadFromDir(env, dir, "attack")) {
+        LOGE("bootstrap: failed to load libattack.so");
+    }
+
+    env->ReleaseStringUTFChars(nativeLibraryDir, dir);
 }
