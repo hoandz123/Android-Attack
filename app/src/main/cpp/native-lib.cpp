917d2775e90550cc89ee4a8c17626b10d044fe9d
Hoan <thanhhoan14082004@gmail.com>
2026-06-01 13:00:01 +0700

    Gọn native-lib.cpp bỏ wrapper và log thừa

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,39 +1,10 @@
 #include <jni.h>
-#include <android/log.h>
 
-#define LOG_TAG "AttackNative"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+static jstring stringFromNative(JNIEnv *env, jobject) { return env->NewStringUTF("Hello from C++ (NDK 28)"); }
 
-namespace {
-
-jstring stringFromNativeImpl(JNIEnv *env, jobject /* thiz */) {
-    LOGI("attack module invoked");
-    return env->NewStringUTF("Hello from C++ (NDK 28)");
-}
-
-const JNINativeMethod kMainActivityMethods[] = {
-    {"stringFromNative", "()Ljava/lang/String;", reinterpret_cast<void *>(stringFromNativeImpl)},
-};
-
-} // namespace
-
-extern "C" JNIEXPORT jboolean JNICALL attack_register_natives(JNIEnv *env) {
-    if (env == nullptr) {
-        return JNI_FALSE;
-    }
-
-    jclass clazz = env->FindClass("com/android/attack/MainActivity");
-    if (clazz == nullptr) {
-        LOGE("FindClass MainActivity failed");
-        return JNI_FALSE;
-    }
-
-    if (env->RegisterNatives(clazz, kMainActivityMethods, sizeof(kMainActivityMethods) / sizeof(kMainActivityMethods[0])) != JNI_OK) {
-        LOGE("RegisterNatives failed");
-        return JNI_FALSE;
-    }
-
-    LOGI("RegisterNatives complete");
-    return JNI_TRUE;
+extern "C" jboolean attack_register_natives(JNIEnv *env) {
+    JNINativeMethod m = {"stringFromNative", "()Ljava/lang/String;", (void *)stringFromNative};
+    jclass c = env->FindClass("com/android/attack/MainActivity");
+    if (!c) return JNI_FALSE;
+    return env->RegisterNatives(c, &m, 1) == JNI_OK ? JNI_TRUE : JNI_FALSE;
 }
